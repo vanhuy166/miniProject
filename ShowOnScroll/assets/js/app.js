623c6390c870aa6757eef2acb10901897ee774fa
vanhuy166 <saikhe166@gmail.com>
2022-06-12 09:10:22 +0700

    Show On Scroll

---
+++ ShowOnScroll/assets/js/app.js
@@ -0,0 +1,29 @@
+let elToShow = document.querySelectorAll('.show-on-scroll')
+
+let isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    console.log(rect)
+
+    let viewHeight = window.innerHeight || document.documentElement.clientHeight
+
+    return (
+        (rect.top <= 0 && rect.bottom >= 0) ||
+        (rect.bottom >= viewHeight && rect.top <= viewHeight) ||
+        (rect.top >= 0 && rect.bottom <= viewHeight)
+    )
+}
+
+function loop() {
+    elToShow.forEach((item) => {
+        if (isElInViewPort(item)) {
+            item.classList.add('start')
+        } else {
+            item.classList.remove('start')
+        }
+    })
+}
+
+window.onscroll = loop
+
+loop()
